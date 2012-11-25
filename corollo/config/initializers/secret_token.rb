332155c1fd90ec85fd306b90518aa1928dd57495
Rahul Tapali <rahul.tapali@infibeam.net>
2012-11-25 17:24:28 +0530

    Creating rails application with mysql database

---
+++ corollo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Corollo::Application.config.secret_token = '84959e8659aa867e3957cdca7b33f703cc6423b23e93601224adacc92487fec125adb9fe3983ffab6744dd8595d721e3b9be436d185b71603fa3844509d35743'
